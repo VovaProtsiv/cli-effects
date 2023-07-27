051cd690d423d8330c1eef97e9836fb760204231
Volodymyr Protsiv <vovapro111@gmail.com>
2023-07-27 18:24:04 +0300

    added readCommandNumber()

--- src/main/scala/dev/protsiv/app/App.scala
+++ src/main/scala/dev/protsiv/app/App.scala
@@ -1,10 +1,12 @@
 package dev.protsiv.app
 
+import cats.MonadError
 import cats.implicits.toShow
 import dev.protsiv.app.Configuration.{AppOp, readEnv}
 import dev.protsiv.app.Syntax.IOOps
 
 object App {
+  val ME = MonadError[AppOp, String]
   def printOptions: AppOp[Unit] = {
     def mkOptionsString(commands: List[Command]): String = {
       val header: String = "Please select an option: \n"
@@ -20,5 +22,13 @@ object App {
       options = mkOptionsString(allCommands)
       _ <- env.console.printLine(options).toAppOp
     } yield ()
+  }
+
+  def readCommandNumber(): AppOp[Int] = {
+    for {
+      env <- readEnv
+      option <- env.console.readLine("Your option: ").toAppOp
+      cmdNumber <- ME.fromOption(option.toIntOption, "Invalid option selected")
+    } yield cmdNumber
   }
 }
--- src/test/scala/dev/protsiv/app/AppSpec.scala
+++ src/test/scala/dev/protsiv/app/AppSpec.scala
@@ -1,26 +1,54 @@
 package dev.protsiv.app
 
 import cats.effect.testing.scalatest.AsyncIOSpec
-import dev.protsiv.app.App.printOptions
+import dev.protsiv.{FakeClient, FakeConsole}
+import dev.protsiv.app.App.{printOptions, readCommandNumber}
 import dev.protsiv.app.CommonSample.{console, env, expectedPrintOptions, printOptionCommands}
+import dev.protsiv.app.Configuration.Environment
 import dev.protsiv.service.LiveController
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class AppSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with BeforeAndAfterEach{
-"printOptions()" -{
-  "should write to the console" in {
-    val environment = env.copy(controller = LiveController(printOptionCommands))
-    printOptions.
-      run(environment).
-      value.
-      asserting(_=> console.linesWritten shouldBe expectedPrintOptions)
+class AppSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with BeforeAndAfterEach {
+  "printOptions()" - {
+    "should write to the console" in {
+      val environment = env.copy(controller = LiveController(printOptionCommands))
+      printOptions.
+        run(environment).
+        value.
+        asserting(_ => console.linesWritten shouldBe expectedPrintOptions)
+    }
   }
-}
+  "readCommandNumber()" - {
+    "when input is valid int" - {
+      "should return Right with a number" in {
+        console.linesToRead = List("3")
+        readCommandNumber().run(env).value.asserting(_ shouldBe Right(3))
+      }
+      "should write to console" in {
+        console.linesToRead = List("3")
+        readCommandNumber().run(env).value.asserting(_ => console.linesWritten shouldBe List("Your option: "))
+      }
+    }
+    "when input isn't valid int" - {
+      "should Left with an error msg" in {
+        console.linesToRead = List("exit")
+        readCommandNumber().run(env).value.asserting(_ shouldBe Left("Invalid option selected"))
+      }
+      "should write to console" in {
+        console.linesToRead =  List("exit")
+        readCommandNumber().run(env).value.asserting(_ => console.linesWritten shouldBe List("Your option: "))
+      }
+    }
+
+  }
 
   override protected def afterEach(): Unit = {
     console.linesWritten = Nil
     super.afterEach()
   }
+
+
+
 }
